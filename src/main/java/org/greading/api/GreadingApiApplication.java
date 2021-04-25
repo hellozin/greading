2b03e9c84c5bd649934f9d903b9b46aeead9f72c
ian.hellozin <ian.hellozin@kakaocorp.com>
2021-04-26 00:04:53 +0900

    Initial commit

---
+++ src/main/java/org/greading/api/GreadingApiApplication.java
@@ -0,0 +1,13 @@
+package org.greading.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreadingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreadingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/greading/api/GreadingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greading.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreadingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
