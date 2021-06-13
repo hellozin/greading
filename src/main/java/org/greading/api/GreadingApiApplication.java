b6ca25acad34cd39a11060c2dfa2fa9003495b60
hellozin <paul26375@gmail.com>
2021-06-13 16:28:40 +0900

    테스트용 데이터 init 추가

--- src/main/java/org/greading/api/GreadingApiApplication.java
+++ src/main/java/org/greading/api/GreadingApiApplication.java
@@ -1,7 +1,16 @@
 package org.greading.api;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.greading.api.selection.Selection;
+import org.greading.api.util.DevelopUtils;
+import org.greading.api.vote.Vote;
+import org.greading.api.vote.VoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class GreadingApiApplication {
@@ -10,4 +19,29 @@ public class GreadingApiApplication {
         SpringApplication.run(GreadingApiApplication.class, args);
     }
 
+}
+
+@Component
+class Runner implements CommandLineRunner {
+
+    @Autowired
+    private VoteService voteService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Selection> selections = new ArrayList<>();
+
+        selections.add(new Selection("first selection"));
+        selections.add(new Selection("second selection"));
+        selections.add(new Selection("third selection"));
+
+        Vote vote = voteService.createVote("first vote", selections);
+        System.out.println("vote id : " + vote.getId());
+
+        List<Selection> all = vote.getSelections();
+        long lastSelectionId = all.get(all.size() - 1).getId();
+
+        voteService.vote(vote.getId(), lastSelectionId, DevelopUtils.generateMockId());
+        voteService.vote(vote.getId(), lastSelectionId, DevelopUtils.generateMockId());
+    }
 }
